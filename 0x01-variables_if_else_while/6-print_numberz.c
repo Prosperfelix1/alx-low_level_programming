a9c9a0d319fb8eb4ef8b7d8789c452cb0ffaa9ea
Prosperfelix1 <prosperfelix85@gmail.com>
2023-03-20 18:22:14 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all single digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
